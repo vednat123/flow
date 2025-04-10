91f773f438790d1cdae81df1aebf6fb19f334b2c
Om Sathe <om.sathe@ontariotechu.net>
2025-04-10 04:31:52 -0400

    Getting rid of pollLogic.js since its unused right now

--- flow/src/pollLogic.js
+++
@@ -1,9 +0,0 @@
-export function createPollData(question, options, allowMultiple) {
-    return {
-      question: question,
-      options: options.filter(opt => opt.trim() !== ''),
-      allowMultiple: allowMultiple,
-      createdAt: new Date().toISOString(),
-    };
-  }
-  
