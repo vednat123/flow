918569e256330ad60d2866dc7be87d8ac09fa323
Andrew Cleeg <cleggster@hotmail.com>
2025-04-09 23:40:09 -0400

    logged in username shows

--- flow/src/components/SidebarMenu.vue
+++ flow/src/components/SidebarMenu.vue
@@ -1,9 +1,20 @@
+<script setup>
+import {useLoggedIn} from '@/stores/storeAccounts'
+import { onMounted } from 'vue'
+
+const userStore = useLoggedIn();
+console.log(userStore.user)
+
+    
+</script>
+
 <template>
   <aside class="sidebar-left">
     <div class="logo-container">
       <router-link to="/home">
         <img src="@/assets/Flow-Logo.jpg" class="flow-logo" />
       </router-link>
+      <p>{{ userStore.user }}</p>
     </div>
     <h2>Menu</h2>
     <ul>
@@ -34,7 +45,8 @@ export default {
   name: 'SidebarLeft',
   data() {
     return {
-      showLogoutConfirm: false
+      showLogoutConfirm: false,
+      user: 'name'
     }
   },
   methods: {
