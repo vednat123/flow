f9275de68a6355aa8654708d41d163a290ca1db3
evedant <vedant.trivedi@ontariotechu.net>
2025-04-10 14:56:17 -0400

    small change

--- flow/src/App.vue
+++ flow/src/App.vue
@@ -1,6 +1,4 @@
-<!-- src/App.vue -->
 <template>
-  <!-- Root container for your app. The RouterView will load either Login or Home, etc. -->
   <router-view />
 </template>
 
@@ -11,6 +9,4 @@ export default {
 </script>
 
 <style>
-/* If you want a global style or wrapper, you could do it here. 
-   But you likely rely on your main.css for general theming. */
 </style>
